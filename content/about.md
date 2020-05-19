53ff51bc28514c0ad50db1a51a9391cc1d8abb0e
Chanh Nguyen <chanhthedj@gmail.com>
2020-05-19 14:08:50 -0600

    add a new field for testing

--- content/about.md
+++ content/about.md
@@ -1,6 +1,7 @@
 ---
 title: About
 date: 2019-03-17T19:31:20.591Z
+dan: It worked!
 ---
 
 ## Welcome to the About page
--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import { attributes, html } from '../content/about.md'
 const About = () => (
   <Layout>
     <h1>{attributes.title}</h1>
+    <h2>{attributes.dan}</h2>
     <div dangerouslySetInnerHTML={{ __html: html }} />
     <style jsx>{`
       h1,
