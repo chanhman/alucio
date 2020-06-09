1c12e96c5a64ea0e360f566eae1c9734ecb96b95
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-09 13:07:47 -0600

    Add the rest of the content for Product

--- pages/product.js
+++ pages/product.js
@@ -1,37 +1,48 @@
 import Layout from '../components/Layout';
-import { attributes, html } from '../content/product.md'
+import { attributes } from '../content/product.md'
 
 const Product = () => (
   <Layout>
     <h1>Product</h1>
 
-    <div>{ attributes.heroHeading }</div>
-    <div>{ attributes.heroDescription }</div>
+    <div>{ attributes.hero__heading }</div>
+    <div>{ attributes.hero__description }</div>
 
-    <div>{ attributes.productOverviewHeading }</div>
+    <div>{ attributes.productOverview__heading }</div>
 
-    <div>{ attributes.keyFeaturesHeading }</div>
-    <div>{ attributes.keyFeaturesDescription }</div>
-    <div>{ attributes.keyFeatureHeading1 }</div>
-    <div>{ attributes.keyFeatureDescription1 }</div>
-    <div>{ attributes.keyFeatureHeading2 }</div>
-    <div>{ attributes.keyFeatureDescription2 }</div>
-    <div>{ attributes.keyFeatureHeading3 }</div>
-    <div>{ attributes.keyFeatureDescription3 }</div>
-    <div>{ attributes.keyFeatureHeading4 }</div>
-    <div>{ attributes.keyFeatureDescription4 }</div>
-    <div>{ attributes.keyFeatureHeading5 }</div>
-    <div>{ attributes.keyFeatureDescription5 }</div>
-    <div>{ attributes.keyFeatureHeading6 }</div>
-    <div>{ attributes.keyFeatureDescription6 }</div>
-    <div>{ attributes.keyFeatureHeading7 }</div>
-    <div>{ attributes.keyFeatureDescription7 }</div>
-    <div>{ attributes.keyFeatureHeading8 }</div>
-    <div>{ attributes.keyFeatureDescription8 }</div>
+    <div>{ attributes.keyFeatures__heading }</div>
+    <div>{ attributes.keyFeatures__description }</div>
+    <div>{ attributes.keyFeature__heading1 }</div>
+    <div>{ attributes.keyFeature__description1 }</div>
+    <div>{ attributes.keyFeature__heading2 }</div>
+    <div>{ attributes.keyFeature__description2 }</div>
+    <div>{ attributes.keyFeature__heading3 }</div>
+    <div>{ attributes.keyFeature__description3 }</div>
+    <div>{ attributes.keyFeature__heading4 }</div>
+    <div>{ attributes.keyFeature__description4 }</div>
+    <div>{ attributes.keyFeature__heading5 }</div>
+    <div>{ attributes.keyFeature__description5 }</div>
+    <div>{ attributes.keyFeature__heading6 }</div>
+    <div>{ attributes.keyFeature__description6 }</div>
+    <div>{ attributes.keyFeature__heading7 }</div>
+    <div>{ attributes.keyFeature__description7 }</div>
+    <div>{ attributes.keyFeature__heading8 }</div>
+    <div>{ attributes.keyFeature__description8 }</div>
 
-    <div>{ attributes.integratedVideoConferencingHeading }</div>
-    <div>{ attributes.integratedVideoConferencingDescription }</div>
-    <div>{ attributes.integratedVideoConferencingCTALabel }</div>
+    <div>{ attributes.integratedVideoConferencing__heading }</div>
+    <div>{ attributes.integratedVideoConferencing__description }</div>
+    <div>{ attributes.integratedVideoConferencing__ctaLabel }</div>
+
+    <div>{ attributes.platformFlexibility__heading }</div>
+    <div>{ attributes.platformFlexibility__description }</div>
+
+    <div>{ attributes.logo1 }</div>
+    <div>{ attributes.logo2 }</div>
+    <div>{ attributes.logo3 }</div>
+    <div>{ attributes.logo4 }</div>
+    <div>{ attributes.logo5 }</div>
+    <div>{ attributes.logo6 }</div>
+    <div>{ attributes.logo7 }</div>
   </Layout>
 )
 
