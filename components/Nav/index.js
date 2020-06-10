4c5af014238d3f490d3de8a329740fe99b5a03fe
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-10 14:48:36 -0600

    Begin styling nav

---
+++ components/Nav/index.js
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import Logo from './logo.svg'
+import Button from '../Button'
+import styles from './index.module.scss'
+import { attributes } from '../../content/navigation.md'
+
+const Nav = () => {
+  let { links } = attributes;
+
+  return (
+    <header className={styles.container}>
+      <div>
+        <Link href="/">
+          <Logo className={styles.logo}/>
+        </Link>
+      </div>
+
+      <nav className={styles.nav}>
+        <ul className={styles.links}>
+          {links.map(link => (
+            <li>
+              <Link href={link.url}>
+                <a>{link.label}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div>
+        <Button label='Request a demo' url='#'/>
+      </div>
+    </header>
+  )
+}
+
+export default Nav
