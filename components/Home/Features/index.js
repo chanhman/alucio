7a4248941848a7e2f4523998dccbb2cc15988f86
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-11 08:06:10 -0600

    Add some  responsive styles to features

--- components/Home/Features/index.js
+++ components/Home/Features/index.js
@@ -3,13 +3,21 @@ import Feature from '../Feature'
 import { attributes } from '../../../content/home.md'
 
 const Features = () => (
-  <>
+  <section className={styles.container}>
     <h3 className={styles.heading}>{attributes.featuresHeading}</h3>
 
-    <Feature description={attributes.featureOne}></Feature>
-    <Feature description={attributes.featureTwo}></Feature>
-    <Feature description={attributes.featureThree}></Feature>
-  </>
+    <div className={styles.features}>
+      <div>
+        <Feature description={attributes.featureOne}></Feature>
+      </div>
+      <div>
+        <Feature description={attributes.featureTwo}></Feature>
+      </div>
+      <div>
+        <Feature description={attributes.featureThree}></Feature>
+      </div>
+    </div>
+  </section>
 )
 
 export default Features;
