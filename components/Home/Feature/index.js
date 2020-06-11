2fca4badc02e3fd02814b8af13d367a26c9d0d88
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-11 07:26:24 -0600

    style feature comp

--- components/Home/Feature/index.js
+++ components/Home/Feature/index.js
@@ -1,5 +1,12 @@
 import styles from './index.module.scss'
 
-const Feature = ({description}) => <div>{description}</div>
+const Feature = ({description}) => (
+  <>
+    <div className={styles.iconWrapper}>
+      <div className={styles.inner}>Icon</div>
+    </div>
+    <p className={styles.description}>{description}</p>
+  </>
+)
 
 export default Feature
