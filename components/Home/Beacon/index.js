0b8358f9b913dc2433b180514c24b3edd8fecb99
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-11 10:59:38 -0600

    Style beacon infographic

--- components/Home/Beacon/index.js
+++ components/Home/Beacon/index.js
@@ -1,4 +1,6 @@
+import classes from 'classnames';
 import styles from './index.module.scss'
+import Dashes from './dashes.svg'
 import { attributes } from '../../../content/home.md'
 
 const Beacon = () => (
@@ -11,22 +13,31 @@ const Beacon = () => (
     <div>
       <div className={styles.infoGraphic}>
         <div className={styles.infoGraphic__cell}>
-          <div className={styles.inner}>Label</div>
+          <div className={styles.inner}>{attributes.infoGraphicLbl1}</div>
         </div>
+        <div className={styles.dashes}><Dashes className={styles.rotate}></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <div className={styles.inner}>Label</div>
+          <div className={styles.inner}>{attributes.infoGraphicLbl2}</div>
         </div>
+        <div className={styles.dashes}><Dashes></Dashes></div>
+        <div></div>
+        <div className={styles.dashes}><Dashes></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <div className={styles.inner}>Label</div>
+          <div className={styles.inner}>{attributes.infoGraphicLbl3}</div>
         </div>
+        <div className={styles.dashes}><Dashes className={styles.rotate}></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <div className={styles.inner}>Label</div>
+          <div className={styles.inner}>{attributes.infoGraphicLbl4}</div>
         </div>
+        <div className={styles.dashes}><Dashes></Dashes></div>
+        <div></div>
+        <div className={styles.dashes}><Dashes></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <div className={styles.inner}>Label</div>
+          <div className={styles.inner}>{attributes.infoGraphicLbl5}</div>
         </div>
+        <div className={styles.dashes}><Dashes className={styles.rotate}></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <div className={styles.inner}>Label</div>
+          <div className={styles.inner}>{attributes.infoGraphicLbl6}</div>
         </div>
       </div>
     </div>
