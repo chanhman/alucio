381ead1c22a1810665201a6598af3442b54f9474
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-11 07:16:32 -0600

    Style features heading

---
+++ components/Home/Feature/index.js
@@ -0,0 +1,5 @@
+import styles from './index.module.scss'
+
+const Feature = ({description}) => <div>{description}</div>
+
+export default Feature
--- components/Home/Features/index.js
+++ components/Home/Features/index.js
@@ -1,11 +1,14 @@
+import styles from './index.module.scss'
+import Feature from '../Feature'
 import { attributes } from '../../../content/home.md'
 
 const Features = () => (
   <>
-    <h3>{attributes.featuresHeading}</h3>
-    <p>{attributes.featureOne}</p>
-    <p>{attributes.featureTwo}</p>
-    <p>{attributes.featureThree}</p>
+    <h3 className={styles.heading}>{attributes.featuresHeading}</h3>
+
+    <Feature description={attributes.featureOne}></Feature>
+    <Feature description={attributes.featureTwo}></Feature>
+    <Feature description={attributes.featureThree}></Feature>
   </>
 )
 
