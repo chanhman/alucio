1d0dda113012653b801c4413e97013831752e28d
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-11 09:37:24 -0600

    Style the text portion of Beacon

--- components/Home/Beacon/index.js
+++ components/Home/Beacon/index.js
@@ -3,9 +3,14 @@ import { attributes } from '../../../content/home.md'
 
 const Beacon = () => (
   <section className={styles.container}>
-    <h2 className={styles.heading}>{attributes.beaconHeading}</h2>
-    <p className={styles.description}>{attributes.beaconDescription}</p>
-    <a className={styles.cta} href={attributes.beaconCtaUrl}>{attributes.beaconCtaLabel}</a>
+    <div>
+      <h2 className={styles.heading}>{attributes.beaconHeading}</h2>
+      <p className={styles.description}>{attributes.beaconDescription}</p>
+      <a className={styles.cta} href={attributes.beaconCtaUrl}>{attributes.beaconCtaLabel}</a>
+    </div>
+    <div>
+      Graphic
+    </div>
   </section>
 )
 
