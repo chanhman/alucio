94196de1149455b2446f384147960e57d38caa56
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-11 06:27:01 -0600

    Create features comp

---
+++ components/Home/Features/index.js
@@ -0,0 +1,12 @@
+import { attributes } from '../../../content/home.md'
+
+const Features = () => (
+  <>
+    <h3>{attributes.featuresHeading}</h3>
+    <p>{attributes.featureOne}</p>
+    <p>{attributes.featureTwo}</p>
+    <p>{attributes.featureThree}</p>
+  </>
+)
+
+export default Features;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import Layout from '../components/Layout'
 import Hero from '../components/Home/Hero'
-import { attributes, html } from '../content/home.md'
+import Features from '../components/Home/Features'
+import { attributes } from '../content/home.md'
 
 const Home = () => (
   <Layout>
     <Hero></Hero>
-    <h3>{attributes.featuresHeading}</h3>
-    <p>{attributes.featureOne}</p>
-    <p>{attributes.featureTwo}</p>
-    <p>{attributes.featureThree}</p>
+    <Features></Features>
     <h3>{attributes.beaconHeading}</h3>
     <p>{attributes.beaconDescription}</p>
-
-    <div dangerouslySetInnerHTML={{ __html: html }} />
   </Layout>
 )
 
