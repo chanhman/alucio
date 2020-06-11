fefcc7c60ab5641a2d539a63ffb4c9d55c825b9f
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-10 21:11:47 -0600

    Style mobile menu

--- components/MobileNav/index.js
+++ components/MobileNav/index.js
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-import cx from 'classnames';
+import Link from 'next/link'
+import Logo from './logo-white.svg'
+import classes from 'classnames';
 import styles from './index.module.scss'
+import { attributes } from '../../content/navigation.md'
 
 const MobileNav = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+  const { links } = attributes;
 
   return (
     <>
@@ -14,9 +18,28 @@ const MobileNav = () => {
         <div></div>
       </div>
 
-      <div className={isOpen ? 'menu active' : 'menu '}>
-        <button onClick={toggle}>Close me</button>
-        Menu
+      <div className={classes(styles.menu, isOpen ? styles.active : '')}>
+        <header className={styles.header}>
+          <div>
+            <Link href="/">
+              <a><Logo className={styles.logo}/></a>
+            </Link>
+          </div>
+          <div>
+            <button className={styles.closeToggle} onClick={toggle}>Close</button>
+          </div>
+        </header>
+        <nav className={styles.nav}>
+          <ul className={styles.links}>
+            {links.map(link => (
+              <li>
+                <Link href={link.url}>
+                  <a>{link.label}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </>
   );
--- components/Nav/index.js
+++ components/Nav/index.js
@@ -6,7 +6,7 @@ import styles from './index.module.scss'
 import { attributes } from '../../content/navigation.md'
 
 const Nav = () => {
-  let { links } = attributes;
+  const { links } = attributes;
 
   return (
     <>
