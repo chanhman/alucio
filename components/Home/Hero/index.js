88c5367092d3d42869042b9e3bc6af20c991f9e6
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-22 16:33:49 -0600

    style video overlay

--- components/Home/Hero/index.js
+++ components/Home/Hero/index.js
@@ -5,9 +5,11 @@ const Hero = () => (
   <section className={styles.container}>
     <h1 className={styles.heading}>{attributes.heroHeading}</h1>
     <h2 className={styles.subheading}>{attributes.heroDescription}</h2>
-    <video playsinline autoplay muted loop poster="" id="">
-      <source src={attributes.heroVideo} type="video/mp4" />
-    </video>
+    <div className={styles.lighten}></div>
+    <div className={styles.multiply}></div>
+    <div className={styles.brighten}></div>
+    <div className={styles.color}></div>
+    <video src={attributes.heroVideo} autoPlay loop muted></video>
   </section>
 )
 
