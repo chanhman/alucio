05caf0a6f4784fbcb4c114383ef70c5e56847b92
Chanh Nguyen <chanhthedj@gmail.com>
2020-06-25 17:11:23 -0600

    Create sectionIntro styles

--- components/Page/SectionIntro/index.js
+++ components/Page/SectionIntro/index.js
@@ -1,9 +1,9 @@
 import styles from './SectionIntro.module.scss'
 
 const SectionIntro = ({heading, description}) => (
-  <section>
-    <h2>{heading}</h2>
-    <p>{description}</p>
+  <section className={styles.container}>
+    <h2 className={styles.heading}>{heading}</h2>
+    <p className={styles.description}>{description}</p>
   </section>
 )
 
