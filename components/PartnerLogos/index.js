e566134c9dceea92d53ebaa3c24752c2298963b4
Chanh Nguyen <chanhthedj@gmail.com>
2020-07-01 15:14:55 -0600

    Create boilerplate Logos comp

---
+++ components/PartnerLogos/index.js
@@ -0,0 +1,11 @@
+import styles from './PartnerLogos.module.scss'
+
+const PartnerLogos = ({data}) => {
+  return (
+    <section className={styles.container}>
+      {/* Loop through logos */}
+    </section>
+  )
+}
+
+export default PartnerLogos
