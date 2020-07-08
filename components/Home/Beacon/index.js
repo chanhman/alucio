b7350aa5c89b964ae147ddddaa46831ef926b91e
Chanh Nguyen <chanhthedj@gmail.com>
2020-07-08 10:23:24 -0600

    Move icon above label

--- components/Home/Beacon/index.js
+++ components/Home/Beacon/index.js
@@ -11,37 +11,49 @@ const Beacon = ({data}) => (
     <div>
       <div className={styles.infoGraphic}>
         <div className={styles.infoGraphic__cell}>
-          <img src={data.beaconInfoGraphicIcon1} alt=""/>
-          <div className={styles.inner}>{data.beaconInfoGraphicLbl1}</div>
+          <div className={styles.inner}>
+            <img src={data.beaconInfoGraphicIcon1} alt=""/>
+            {data.beaconInfoGraphicLbl1}
+          </div>
         </div>
         <div className={styles.dashes}><Dashes className={styles.rotate}></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <img src={data.beaconInfoGraphicIcon2} alt=""/>
-          <div className={styles.inner}>{data.beaconInfoGraphicLbl2}</div>
+          <div className={styles.inner}>
+            <img src={data.beaconInfoGraphicIcon2} alt=""/>
+            {data.beaconInfoGraphicLbl2}
+          </div>
         </div>
         <div className={styles.dashes}><Dashes></Dashes></div>
         <div></div>
         <div className={styles.dashes}><Dashes></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <img src={data.beaconInfoGraphicIcon3} alt=""/>
-          <div className={styles.inner}>{data.beaconInfoGraphicLbl3}</div>
+          <div className={styles.inner}>
+            <img src={data.beaconInfoGraphicIcon3} alt=""/>
+            {data.beaconInfoGraphicLbl3}
+          </div>
         </div>
         <div className={styles.dashes}><Dashes className={styles.rotate}></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <img src={data.beaconInfoGraphicIcon4} alt=""/>
-          <div className={styles.inner}>{data.beaconInfoGraphicLbl4}</div>
+          <div className={styles.inner}>
+            <img src={data.beaconInfoGraphicIcon4} alt=""/>
+            {data.beaconInfoGraphicLbl4}
+          </div>
         </div>
         <div className={styles.dashes}><Dashes></Dashes></div>
         <div></div>
         <div className={styles.dashes}><Dashes></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <img src={data.beaconInfoGraphicIcon5} alt=""/>
-          <div className={styles.inner}>{data.beaconInfoGraphicLbl5}</div>
+          <div className={styles.inner}>
+            <img src={data.beaconInfoGraphicIcon5} alt=""/>
+            {data.beaconInfoGraphicLbl5}
+          </div>
         </div>
         <div className={styles.dashes}><Dashes className={styles.rotate}></Dashes></div>
         <div className={styles.infoGraphic__cell}>
-          <img src={data.beaconInfoGraphicIcon6} alt=""/>
-          <div className={styles.inner}>{data.beaconInfoGraphicLbl6}</div>
+          <div className={styles.inner}>
+            <img src={data.beaconInfoGraphicIcon6} alt=""/>
+            {data.beaconInfoGraphicLbl6}
+          </div>
         </div>
       </div>
     </div>
