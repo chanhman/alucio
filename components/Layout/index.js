0b455631c19944fc7f36e8cccd26f98a2391b1f9
Chanh Nguyen <chanhthedj@gmail.com>
2020-07-13 15:32:29 -0600

    Add Olark embed code

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -13,6 +13,16 @@ const Layout = ({ children }) => (
     <main>{children}</main>
 
     <Footer/>
+
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+      ;(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");
+      /* custom configuration goes here (www.olark.com/documentation) */
+      olark.identify('9066-901-10-6140');
+    `,
+      }}
+    />
   </>
 )
 
